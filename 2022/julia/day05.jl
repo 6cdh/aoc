f499605f7628e20c38f161e3a4ac25026f3257a9
6cdh <cd6cdh@gmail.com>
2022-12-05 17:47:39 +0800

    improve Julia day05

--- 2022/julia/day05.jl
+++ 2022/julia/day05.jl
@@ -1,9 +1,9 @@
 function rearrange(stacks, procedure, transformer)
     for p in procedure
         k, from, to = p
-        moved = stacks[from][end-k+1:end]
+        moved = transformer(stacks[from][end-k+1:end])
         stacks[from] = stacks[from][1:end-k]
-        stacks[to] = vcat(stacks[to], transformer(moved))
+        append!(stacks[to], moved)
     end
 
     println(join(map(last, stacks)))
@@ -16,7 +16,6 @@ function day05()
     n = length(lines[1])
 
     stacks::Vector{Vector{Char}} = fill([], (n + 1) ÷ 4)
-
     for i in m-1:-1:1
         line = lines[i]
         for j in 2:4:n
@@ -28,13 +27,13 @@ function day05()
 
     procs::Vector{NTuple{3,Int}} = []
     for i in m+2:length(lines)
-        words = lines[i] |> split
+        words = split(lines[i])
         k, from, to = parse.(Int, (words[2], words[4], words[6]))
         push!(procs, (k, from, to))
     end
 
-    rearrange(deepcopy(stacks), procs, reverse)
     rearrange(deepcopy(stacks), procs, identity)
+    rearrange(deepcopy(stacks), procs, reverse)
 end
 
 @time day05()
