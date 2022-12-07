bdd09a5d11c1b11a6f58ff48f81e43d64784f878
6cdh <cd6cdh@gmail.com>
2022-12-07 16:29:10 +0800

    improve Racket day07

--- 2022/racket/day07.rkt
+++ 2022/racket/day07.rkt
@@ -4,21 +4,17 @@
 
 (define (read-fs)
   (let ([lines (read-lines)]
+        ;; fs : (Hash string (or Int fs))
         [fs (make-hash (list (cons "/" (make-hash))))]
         [pwd '("/")])
     (for ([line lines])
-      (match (string-ref line 0)
-        [#\$ (when (char=? #\c (string-ref line 2))
-               (match (substring line 5)
-                 ["/" (set! pwd '("/"))]
-                 [".." (set! pwd (cdr pwd))]
-                 [dir (set! pwd (cons dir pwd))]))]
-        [#\d (let ([name (second (string-split line))])
-               (insert! fs (reverse pwd) name (make-hash)))]
-        [_ (let* ([ws (string-split line)]
-                  [size (string->number (first ws))]
-                  [name (second ws)])
-             (insert! fs (reverse pwd) name size))]))
+      (match (string-split line)
+        [(list "$" "cd" "/") (set! pwd '("/"))]
+        [(list "$" "cd" "..") (set! pwd (cdr pwd))]
+        [(list "$" "cd" dir) (set! pwd (cons dir pwd))]
+        [(list "$" "ls") (void)]
+        [(list "dir" dir) (insert! fs (reverse pwd) dir (make-hash))]
+        [(list size file) (insert! fs (reverse pwd) file (string->number size))]))
     fs))
 
 (define (insert! fs pwd name val)
@@ -31,9 +27,8 @@
 (define (for-each-dir-size fs fn)
   (if (number? fs)
       fs
-      (let ([size 0])
-        (for ([(_ val) fs])
-          (set! size (+ size (for-each-dir-size val fn))))
+      (let ([size (for/sum ([(_ val) fs])
+                    (for-each-dir-size val fn))])
         (fn size)
         size)))
 
@@ -47,10 +42,12 @@
     (let* ([root (for-each-dir-size fs identity)]
            [at-least-free (- #e3e7 (- #e7e7 root))]
            [ans root])
-      (for-each-dir-size fs (λ (s) (when (>= s at-least-free)
-                                     (set! ans (min ans s)))))
+      (for-each-dir-size fs
+                         (λ (s) (when (>= s at-least-free)
+                                  (set! ans (min ans s)))))
       (println ans))))
 
 (time (day07))
+
 
 
