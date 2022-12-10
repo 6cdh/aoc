0a6ea41b310caabc760bc8ce28dd973868065a8b
6cdh <cd6cdh@gmail.com>
2022-12-10 11:58:20 +0800

    improve Racket day09

--- 2022/racket/day09.rkt
+++ 2022/racket/day09.rkt
@@ -19,29 +19,30 @@
           [else (move-knot cur (cons (sgn Δx) (sgn Δy)))])))
 
 ;; move k steps
-(define (move-rope rope Δ k visited)
+(define (move-rope! rope Δ k visited)
   (let ([n (vector-length rope)])
     (for ([_ k])
       (aupd! rope 0 (λ (old) (move-knot old Δ)))
       ;; update the whole rope
-      (for ([prev n]
-            [i (range 1 n)])
-        (aupd! rope i (λ (old) (adjust-knot (aref rope prev) old))))
+      (for ([i (range 1 n)])
+        (aupd! rope i
+               (λ (old) (adjust-knot (aref rope (sub1 i))
+                                     old))))
       (set-add! visited (aref rope (sub1 n))))
     rope))
 
 (define (emulate lines n)
-  (let ([visited (mutable-set '(0 . 0))])
-    (for/fold ([rope (make-array n '(0 . 0))])
-              ([line lines])
+  (let ([visited (mutable-set '(0 . 0))]
+        [rope (make-array n '(0 . 0))])
+    (for ([line lines])
       (let* ([ws (string-split line)]
              [dir (first ws)]
              [k (string->number (second ws))])
         (match dir
-          ["L" (move-rope rope '(-1 . 0) k visited)]
-          ["R" (move-rope rope '(1 . 0) k visited)]
-          ["U" (move-rope rope '(0 . 1) k visited)]
-          ["D" (move-rope rope '(0 . -1) k visited)])))
+          ["L" (move-rope! rope '(-1 . 0) k visited)]
+          ["R" (move-rope! rope '(1 . 0) k visited)]
+          ["U" (move-rope! rope '(0 . 1) k visited)]
+          ["D" (move-rope! rope '(0 . -1) k visited)])))
     (set-count visited)))
 
 (define (day09)
@@ -50,5 +51,4 @@
     (println (emulate lines 10))))
 
 (time (day09))
-
 
