d6a36baa48d804940cb2f328f8601e325a0e4453
6cdh <cd6cdh@gmail.com>
2022-12-10 11:38:32 +0800

    adopt ChatGPT's suggestion for day09 * rename variable old -> old-pos * rename function move1 -> move-knot * rename function adjust -> adjust-knot * rename function movek -> move-rope

--- 2022/racket/day09.rkt
+++ 2022/racket/day09.rkt
@@ -3,30 +3,30 @@
 (require "lib.rkt")
 
 ;; move 1 step
-(define (move1 old dir)
-  (cons (+ (car old) (car dir))
-        (+ (cdr old) (cdr dir))))
+(define (move-knot old-pos dir)
+  (cons (+ (car old-pos) (car dir))
+        (+ (cdr old-pos) (cdr dir))))
 
 ;; return the new position of `cur`
 ;; prev: new position of the previous knot
 ;; cur: old position of the current knot
-(define (adjust prev cur)
+(define (adjust-knot prev cur)
   (let ([Δx (- (car prev) (car cur))]
         [Δy (- (cdr prev) (cdr cur))])
     (cond [(and (<= (abs Δx) 1)
                 (<= (abs Δy) 1))
            cur]
-          [else (move1 cur (cons (sgn Δx) (sgn Δy)))])))
+          [else (move-knot cur (cons (sgn Δx) (sgn Δy)))])))
 
 ;; move k steps
-(define (movek rope Δ k visited)
+(define (move-rope rope Δ k visited)
   (let ([n (vector-length rope)])
     (for ([_ k])
-      (aupd! rope 0 (λ (old) (move1 old Δ)))
+      (aupd! rope 0 (λ (old) (move-knot old Δ)))
       ;; update the whole rope
       (for ([prev n]
             [i (range 1 n)])
-        (aupd! rope i (λ (old) (adjust (aref rope prev) old))))
+        (aupd! rope i (λ (old) (adjust-knot (aref rope prev) old))))
       (set-add! visited (aref rope (sub1 n))))
     rope))
 
@@ -38,10 +38,10 @@
              [dir (first ws)]
              [k (string->number (second ws))])
         (match dir
-          ["L" (movek rope '(-1 . 0) k visited)]
-          ["R" (movek rope '(1 . 0) k visited)]
-          ["U" (movek rope '(0 . 1) k visited)]
-          ["D" (movek rope '(0 . -1) k visited)])))
+          ["L" (move-rope rope '(-1 . 0) k visited)]
+          ["R" (move-rope rope '(1 . 0) k visited)]
+          ["U" (move-rope rope '(0 . 1) k visited)]
+          ["D" (move-rope rope '(0 . -1) k visited)])))
     (set-count visited)))
 
 (define (day09)
@@ -50,4 +50,5 @@
     (println (emulate lines 10))))
 
 (time (day09))
+
 
