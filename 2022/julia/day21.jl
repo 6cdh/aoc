3c6f5e10073cd2f20a9aed1030ac2779b0099959
6cdh <cd6cdh@gmail.com>
2022-12-21 14:43:06 +0800

    Improve Julia day21

--- 2022/julia/day21.jl
+++ 2022/julia/day21.jl
@@ -1,5 +1,7 @@
 using Match
 
+# try to solve the unknown variable given 2 of 3 variables
+# or return nothing if it can't
 # x op y = result
 function solve_relation(x, op, y, result)
     # map op to op1 that x = result op1 y
@@ -51,38 +53,30 @@ function readinput()
     monkeys
 end
 
-function get_answer(name, monkeys)
-    action = get(monkeys, name, nothing)
-    if isnothing(action)
-        nothing
-    elseif isa(action, Int)
-        action
-    else
-        ans1 = get_answer(action[2], monkeys)
-        ans2 = get_answer(action[3], monkeys)
-        solve_relation(ans1, action[1], ans2, nothing)
-    end
-end
-
-function solve_to_meet(name, monkeys, expect)
+function solve_equations(name, monkeys, expect)
     action = get(monkeys, name, nothing)
     if isnothing(action)
         expect
     elseif isa(action, Int)
         action
+    elseif isnothing(expect)
+        solve_relation(
+            solve_equations(action[2], monkeys, nothing),
+            action[1],
+            solve_equations(action[3], monkeys, nothing),
+            nothing)
     else
-        ans1 = get_answer(action[2], monkeys)
-        ans2 = get_answer(action[3], monkeys)
-        if !isnothing(ans1) && !isnothing(ans2)
-            solve_relation(ans1, action[1], ans2, nothing)
-        elseif isnothing(ans1)
+        ans1 = solve_equations(action[2], monkeys, nothing)
+        ans2 = solve_equations(action[3], monkeys, nothing)
+        @assert(!(isnothing(ans1) && isnothing(ans2)))
+        if isnothing(ans1)
             ans1_expect = solve_relation(ans1, action[1], ans2, expect)
-            solve_to_meet(action[2], monkeys, ans1_expect)
+            solve_equations(action[2], monkeys, ans1_expect)
         elseif isnothing(ans2)
             ans2_expect = solve_relation(ans1, action[1], ans2, expect)
-            solve_to_meet(action[3], monkeys, ans2_expect)
+            solve_equations(action[3], monkeys, ans2_expect)
         else
-            error("NO! both operands depend on `humn`")
+            error("NO!!!! both operands depend on `humn`")
         end
     end
 end
@@ -90,12 +84,13 @@ end
 function day21()
     monkeys = readinput()
 
-    println(get_answer("root", monkeys))
+    println(solve_equations("root", monkeys, nothing))
 
-    monkeys["root"] = (==, monkeys["root"][2], monkeys["root"][3])
+    root_action = monkeys["root"]
+    monkeys["root"] = (==, root_action[2], root_action[3])
     delete!(monkeys, "humn")
 
-    println(solve_to_meet("root", monkeys, true))
+    println(solve_equations("root", monkeys, true))
 end
 
 @time day21()
