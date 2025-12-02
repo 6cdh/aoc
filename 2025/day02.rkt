29649e5341ac26b0a76138ed0a172daed2e21278
6cdh <cd6cdh@gmail.com>
2025-12-02 21:59:40 +0800

    add another method, and improve performance

--- 2025/day02.rkt
+++ 2025/day02.rkt
@@ -13,19 +13,47 @@
     (for/list ([str-range str-range-list])
       (match-define (list start end) (string-split str-range "-"))
       (Range (string->number start) (string->number end))))
-  (define ans1 (sum-invalid-ids-for-ranges range-list #px"^(.+?)(\\1)$"))
-  (define ans2 (sum-invalid-ids-for-ranges range-list #px"^(.+?)(\\1)+$"))
-  (values ans1 ans2))
+  ; (define ans1 (parallel-run (sum-ranges range-list (invalid-id/regex? #px"^(.+?)(\\1)$"))))
+  ; (define ans2 (parallel-run (sum-ranges range-list (invalid-id/regex? #px"^(.+?)(\\1)+$"))))
+  (define ans1 (parallel-run (sum-ranges range-list (invalid-id/string? even?))))
+  (define ans2 (parallel-run (sum-ranges range-list (invalid-id/string? (curryr >= 2)))))
+  (values (thread-wait ans1) (thread-wait ans2)))
 
-(define (sum-invalid-ids-for-ranges range-list pattern)
+;; using regular expressions.
+;; slower but simpler
+(define ((invalid-id/regex? pattern) id)
+  (regexp-match-exact? pattern (number->string id)))
+
+;; using manual string matching.
+;; complex but faster (10x)
+(define ((invalid-id/string? pred) id)
+  (pred (max-repeat-times id)))
+
+(define (sum-ranges range-list invalid-id?)
   (for/sum ([range (in-list range-list)])
-    (sum-invalid-ids-for-range range pattern)))
+    (sum-range range invalid-id?)))
 
-(define (sum-invalid-ids-for-range range pattern)
-  (for/sum ([id (in-range (Range-start range) (add1 (Range-end range)))]
-            #:when (invalid-id? id pattern))
+(define (sum-range range invalid-id?)
+  (for/sum ([id (in-inclusive-range (Range-start range) (Range-end range))]
+            #:when (invalid-id? id))
     id))
 
-(define (invalid-id? id pattern)
-  (regexp-match-exact? pattern (number->string id)))
+(define (max-repeat-times id)
+  (define str (number->string id))
+  (define n (string-length str))
+  (or (for/first ([pat-size (in-inclusive-range 1 (quotient n 2))]
+                  #:when (and (= 0 (modulo n pat-size))
+                              (repeat? str pat-size)))
+        (quotient n pat-size))
+      1))
+
+(define (repeat? str pat-size)
+  (define n (string-length str))
+  (for*/and ([i (in-range 0 pat-size)]
+             [j (in-range i n pat-size)])
+    (char=? (string-ref str i) (string-ref str j))))
+
+(define-syntax-rule (parallel-run expr)
+  (thread #:pool 'own #:keep 'results
+          (lambda () expr)))
 
