9e07d172574ba46e81bbee893ec29e00f2e5dcd2
6cdh <cd6cdh@gmail.com>
2025-12-03 10:40:57 +0800

    add faster implementation for day2

--- 2025/day02.rkt
+++ 2025/day02.rkt
@@ -17,14 +17,20 @@
       (Range (string->number start) (string->number end))))
   ; (define ans1 (parallel-run (sum-ranges range-list (invalid-id/regex? #px"^(.+?)(\\1)$"))))
   ; (define ans2 (parallel-run (sum-ranges range-list (invalid-id/regex? #px"^(.+?)(\\1)+$"))))
-  (define ans1 (parallel-run (sum-ranges range-list (invalid-id/string? even?))))
-  (define ans2 (parallel-run (sum-ranges range-list (invalid-id/string? (curryr >= 2)))))
+  ; (define ans1 (parallel-run (sum-ranges range-list (invalid-id/string? even?))))
+  ; (define ans2 (parallel-run (sum-ranges range-list (invalid-id/string? (curryr >= 2)))))
+  (define ans1 (parallel-run (count-invalid-ids-in-ranges range-list 2)))
+  (define ans2 (parallel-run (count-invalid-ids-in-ranges range-list +inf.0)))
   (values (thread-wait ans1) (thread-wait ans2)))
+
+;; == regular expressions (2 seconds) ==
 
 ;; using regular expressions.
 ;; slower but simpler
 (define ((invalid-id/regex? pattern) id)
   (regexp-match-exact? pattern (number->string id)))
+
+;; == manual string matching (200 ms) ==
 
 ;; using manual string matching.
 ;; complex but faster (10x)
@@ -54,4 +60,34 @@
   (for*/and ([i (in-range 0 pat-size)]
              [j (in-range i n pat-size)])
     (char=? (string-ref str i) (string-ref str j))))
+
+;; == generate all invalid ids and query (40 ms) ==
+
+(define (generate-all-invalid-ids digits max-repeat)
+  (for*/list ([width (in-inclusive-range 1 (quotient digits 2))]
+              [repeat (in-inclusive-range 2 (min max-repeat (quotient digits width)))]
+              [num (in-range (expt 10 (sub1 width)) (expt 10 width))])
+    (compose-id num width repeat)))
+
+(define (compose-id num width repeat)
+  (for/fold ([id num])
+            ([_ (in-range 1 repeat)])
+    (+ (* id (expt 10 width)) num)))
+
+(define (count-invalid-ids-in-ranges range-list max-repeat)
+  (define max-digits
+    (for/max 0 ([range (in-list range-list)])
+      (max (decimal-digits (Range-start range)) (decimal-digits (Range-end range)))))
+  (define all-invalid-ids
+    (~> ids (generate-all-invalid-ids max-digits max-repeat)
+        (remove-duplicates ids)
+        (sort ids <)
+        (list->vector ids)))
+  (define range-sum (vector-range-sum-querier all-invalid-ids))
+  (define n (vector-length all-invalid-ids))
+  (for/sum ([range (in-list range-list)])
+    (define start (vector-binary-search-first all-invalid-ids (curryr >= (Range-start range))))
+    (define end (vector-binary-search-last all-invalid-ids (curryr <= (Range-end range))))
+    ;; throw an error if `start` or `end` not found instead of return wrong answer.
+    (range-sum start end)))
 
--- 2025/utils.rkt
+++ 2025/utils.rkt
@@ -1,8 +1,88 @@
-#lang racket/base
+#lang racket
 
-(provide parallel-run)
+(provide parallel-run
+         ~>
+         binary-search-first
+         vector-binary-search-first
+         binary-search-last
+         vector-binary-search-last
+         vector-range-sum-querier
+         vector-range-sum
+         decimal-digits
+         for/max)
 
 (define-syntax-rule (parallel-run expr)
   (thread #:pool 'own #:keep 'results
           (lambda () expr)))
 
+(define-syntax-rule (~> name exprs ...)
+  (let* ([name exprs] ...)
+    name))
+
+;; for a range [beg ... end],
+;; apply `pred` to get a list of booleans:
+;; [#f ... #t ...]
+;; binary-search-first returns the first index that is #t,
+;; if not found, return #f.
+;; It won't work if the range does not match this pattern.
+;; Require beg <= end.
+(define (binary-search-first beg end pred)
+  (let loop ([from beg] [to end])
+    (define mid (quotient (+ from to) 2))
+    (cond [(= from to) (if (pred from) from #f)]
+          [(pred mid) (loop from mid)]
+          [else (loop (add1 mid) to)])))
+
+(define (vector-binary-search-first vec pred)
+  (define n (vector-length vec))
+  (if (= n 0)
+      #f
+      (binary-search-first 0 (sub1 n) (λ (i) (pred (vector-ref vec i))))))
+
+;; for a range [beg ... end],
+;; apply `pred` to get a list of booleans:
+;; [#t ... #f ...]
+;; binary-search-last returns the last index that is #t,
+;; if not found, return #f.
+;; It won't work if the range does not match this pattern.
+;; Require beg <= end.
+(define (binary-search-last from to pred)
+  (define first-not (binary-search-first from to (compose not pred)))
+  (cond [(false? first-not) to]
+        [(= first-not from) #f]
+        [else (sub1 first-not)]))
+
+(define (vector-binary-search-last vec pred)
+  (define n (vector-length vec))
+  (if (= n 0)
+      #f
+      (binary-search-last 0 (sub1 n) (λ (i) (pred (vector-ref vec i))))))
+
+;; Return a function that calculates range sum of `vec` in range `[start, end]`.
+(define (vector-range-sum-querier vec)
+  (define n (vector-length vec))
+  (define prefix-sum (make-vector n 0))
+  (vector-set! prefix-sum 0 (vector-ref vec 0))
+  (for ([i (in-range 1 n)])
+    (vector-set! prefix-sum i (+ (vector-ref vec i) (vector-ref prefix-sum (sub1 i)))))
+  (λ (start end)
+    (- (vector-ref prefix-sum end)
+       (if (= start 0)
+           0
+           (vector-ref prefix-sum (sub1 start))))))
+
+(define (vector-range-sum vec start end)
+  (for/sum ([i (in-inclusive-range start end)])
+    (vector-ref vec i)))
+
+(define (decimal-digits d)
+  (ceiling (log d 10)))
+
+(define-syntax-rule
+  (for/max init clauses
+           body ...
+           last-expr)
+  (for/fold ([ans init])
+            clauses
+    body ...
+    (max ans last-expr)))
