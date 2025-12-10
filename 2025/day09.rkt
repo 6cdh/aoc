4e93f839abcf1c230af341245baf96d1e305a269
6cdh <cd6cdh@gmail.com>
2025-12-10 12:37:58 +0800

    speedup day9: 70s->18s

--- 2025/day09.rkt
+++ 2025/day09.rkt
@@ -53,10 +53,10 @@
        (point-not-outside? (Pos x2 y2) polygon)
        (point-not-outside? (Pos x1 y2) polygon)
        (point-not-outside? (Pos x2 y1) polygon)
-       (line-not-outside? x1 y1 x1 y2 polygon)
-       (line-not-outside? x1 y2 x2 y2 polygon)
-       (line-not-outside? x2 y2 x2 y1 polygon)
-       (line-not-outside? x2 y1 x1 y1 polygon)))
+       (line-not-outside-fast? x1 y1 x1 y2 polygon)
+       (line-not-outside-fast? x1 y2 x2 y2 polygon)
+       (line-not-outside-fast? x2 y2 x2 y1 polygon)
+       (line-not-outside-fast? x2 y1 x1 y1 polygon)))
 
 (define (line-not-outside? x1 y1 x2 y2 polygon)
   (define speedup 2)
@@ -68,31 +68,47 @@
       (for*/and ([x (in-inclusive-range x1 x2 dx)])
         (point-not-outside? (Pos x y1) polygon))))
 
-(define (point-not-outside? point polygon)
-  (or (point-on-edge? point polygon)
-      (point-inside? point polygon)))
+(define (line-not-outside-fast? x1 y1 x2 y2 polygon)
+  (define dx (sgn (- x2 x1)))
+  (define dy (sgn (- y2 y1)))
+  (if (= dx 0)
+      (line-not-outside2? (min y1 y2) (max y1 y2) (λ (y) (Pos x1 y)) polygon)
+      (line-not-outside2? (min x1 x2) (max x1 x2) (λ (x) (Pos x y1)) polygon)))
+
+(define (line-not-outside2? from to build-pos polygon)
+  (cond [(> from to) #t]
+        [(= from to)
+         (point-not-outside? (build-pos from) polygon)]
+        [(<= (abs (- to from)) 5000)
+         (for/and ([i (in-inclusive-range from to)])
+           (point-not-outside? (build-pos i) polygon))]
+        [else
+         (define mid (quotient (+ from to) 2))
+         (cond [(not (point-not-outside? (build-pos mid) polygon))
+                #f]
+               [else
+                (define th1 (parallel-run (line-not-outside2? from (sub1 mid) build-pos polygon)))
+                (define th2 (parallel-run (line-not-outside2? (add1 mid) to build-pos polygon)))
+                (and (thread-wait th1) (thread-wait th2))])]))
 
 ;; ray casting algorithm
-(define (point-inside? point polygon)
+(define (point-not-outside? point polygon)
   (match-define (Pos x y) point)
-  (for/fold ([inside? #f])
-            ([v1 (in-list polygon)]
-             [v2 (in-list (cdr polygon))])
-    (match-define (Pos x1 y1) v1)
-    (match-define (Pos x2 y2) v2)
-    (if (and (<= (add1 (min y1 y2)) y (max y1 y2))
-             (< x (intersect-x x y x1 y1 x2 y2)))
-        (not inside?)
-        inside?)))
+  (let/cc return
+    (for/fold ([inside? #f])
+              ([v1 (in-list polygon)]
+               [v2 (in-list (cdr polygon))])
+      (match-define (Pos x1 y1) v1)
+      (match-define (Pos x2 y2) v2)
 
-(define (point-on-edge? point polygon)
-  (match-define (Pos x y) point)
-  (for/or ([v1 (in-list polygon)]
-           [v2 (in-list (cdr polygon))])
-    (match-define (Pos x1 y1) v1)
-    (match-define (Pos x2 y2) v2)
-    (or (and (= x1 x x2) (<= (min y1 y2) y (max y1 y2)))
-        (and (= y1 y y2) (<= (min x1 x2) x (max x1 x2))))))
+      (when (or (and (= x1 x x2) (<= (min y1 y2) y (max y1 y2)))
+                (and (= y1 y y2) (<= (min x1 x2) x (max x1 x2))))
+        (return #t))
+
+      (if (and (<= (add1 (min y1 y2)) y (max y1 y2))
+               (< x (intersect-x x y x1 y1 x2 y2)))
+          (not inside?)
+          inside?))))
 
 ;; x of the intersection point of
 ;; the ray from (x ,y) and the line segment from (x1, y1) to (x2, y2)
